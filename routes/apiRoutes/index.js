c8b75fed678e42dd9b09b9154b1fa8f89aae8e82
Dr.Worm <sky.jackson@icloud.com>
2022-05-30 19:24:43 -0400

    updating routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const jokeRoutes = require('.')
+const jokeRoutes = require('./joke-routes');
 
 router.use('/users', userRoutes);
 router.use('/jokes', jokeRoutes);
