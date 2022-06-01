6583b136f0db570929023cc3ef0a2f1c3f6f2cc8
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-05-31 20:36:53 -0400

    initial setup of login logout functionality

--- routes/apiRoutes/user-routes.js
+++ routes/apiRoutes/user-routes.js
@@ -42,40 +42,81 @@ router.get('/:id', (req, res) => {
 });
 
 
-// POST users
-router.post('/', (req, res) => {
-    User.create({
+// CREATE new user
+router.post('/', async (req, res) => {
+    try {
+      const dbUserData = await User.create({
         username: req.body.username,
-        password: req.body.password
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+        password: req.body.password,
+      });
+  
+      // Set up sessions with a 'loggedIn' variable set to `true`
+      req.session.save(() => {
+        req.session.username = dbUserData.username,
+        req.session.loggedIn = true;
+  
+        res.status(200).json(dbUserData);
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 
-// POST login
-router.post('/login', (req, res) => {
-    User.findOne({
+// Login
+router.post('/login', async (req, res) => {
+    try {
+      const dbUserData = await User.findOne({
         where: {
-            username: req.body.username
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that username!'});
-            return;
-        }
-        const validPassword = dbUserData.checkPassword(req.body.password);
-        if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!'});
-            return;
-        }
-        res.json({ user: dbUserData, message: 'You are now logged in!'});
-    });
-});
+          username: req.body.username,
+        },
+      });
+  
+      if (!dbUserData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
+      }
+  
+      const validPassword = await dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect username or password. Please try again!' });
+        return;
+      }
+  
+      // Once the user successfully logs in, set up the sessions variable 'loggedIn'
+      req.session.save(() => {
+        req.session.loggedIn = true;
+  
+        res
+          .status(200)
+          .json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+  
+  // Logout
+  router.post('/logout', (req, res) => {
+    // When the user logs out, destroy the session
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
+  
 
 
 
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
-const bodyParser = require('body-parser');
+const path = require('path');
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+const bodyParser = require("body-parser");
 
+const exphbs = require("express-handlebars");
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set up sessions
+const sess = {
+  secret: "Super secret secret",
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
+  app.listen(PORT, () => console.log("Now listening on PORT " + PORT));
 });
