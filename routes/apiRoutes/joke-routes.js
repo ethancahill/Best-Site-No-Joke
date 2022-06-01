f1155f5a93fa894aa69891dd91fadbcd0dfde70e
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-05-31 21:03:12 -0400

    created dad joke get

--- routes/apiRoutes/joke-routes.js
+++ routes/apiRoutes/joke-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const { User, Joke } = require('../../models');
+const dadJoke = require('@mikemcbride/dad-jokes')
 
 
 // GET dad_joke from npm so the front-end can append it to page
 
 
 // GET jokes of user 
-
+router.get('/dad', (req, res) => {
+    res.json( { joke: dadJoke.random()})
+})
 
 // POST selected joke / image / user info to joke table
+
+module.exports = router
--- routes/htmlRoutes/login.js
+++ routes/htmlRoutes/login.js
@@ -1,3 +1,4 @@
 const router = require('express').Router();
 const { Joke, User } = require('../../models');
 
+module.exports = router
