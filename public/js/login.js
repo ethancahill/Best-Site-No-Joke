025b8b538c808f6587ed5c0cf67d9871dec31fd5
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-01 23:52:45 -0400

    adjusted query selectors in front end login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#login-un').value.trim();
+    const password = document.querySelector('#login-pw').value.trim();
   
     if (username && password) {
       const response = await fetch('/api/users/login', {
@@ -22,8 +22,8 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const username = document.querySelector('#signup-un').value.trim();
+    const password = document.querySelector('#signup-pw').value.trim();
   
     if (username && password) {
       const response = await fetch('/api/users', {
