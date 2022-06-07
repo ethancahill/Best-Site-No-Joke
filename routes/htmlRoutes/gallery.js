aa5fbebf1c1978535a9077ad1a07804485e57956
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-07 16:20:48 -0400

    changed gallery

--- routes/htmlRoutes/gallery.js
+++ routes/htmlRoutes/gallery.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/gallery.html'));
-});
-
-router.get('/', (req, res) => {
     res.render("gallery")
 })
 
