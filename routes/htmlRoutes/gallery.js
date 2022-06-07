87aae6892e6bcf5b1bf41ed4166d77cec96ed11d
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-07 16:34:54 -0400

    reroutes to login if not logged in

--- routes/htmlRoutes/gallery.js
+++ routes/htmlRoutes/gallery.js
@@ -1,8 +1,14 @@
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
 
-router.get('/', (req, res) => {
-    res.render("gallery")
-})
 
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
+    res.render("gallery", {
+      generateCSS: true,
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
 module.exports = router;
