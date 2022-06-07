f5af0a5849a91136cbd7ac3d66438397d1975515
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-06-07 19:02:15 -0400

    generate jokes and images

--- public/js/script.js
+++ public/js/script.js
@@ -5,63 +5,66 @@ var jokeBtn = document.getElementById('jokeBtn');
 
 
 var imgArray = [
- './images/ballons.jpeg',
- './images/beaker.jpg',
- './images/bridge.jpg',
- './images/cat.jpg',
- './images/chicken.jpg',
- './images/cow.jpg',
- './images/face.jpg',
- './images/face2.jpg',
- './images/fellowship.webp',
-'./images/food.jpg',
-'./images/handy.jpg',
-'./images/keys.png',
-'./images/lovepik.jpeg',
-'./images/mordor.webp',
-'./images/rocks.jpg',
-'./images/standing.jpeg',
-'./images/ties.jpg',
-'./images/tools.jpg',
-'./images/tools2.jpg',
-'./images/tow-mater.jpeg',
-'./images/trooper.jpg',
-'./images/witcher.webp'
+    './images/ballons.jpeg',
+    './images/beaker.jpg',
+    './images/bridge.jpg',
+    './images/cat.jpg',
+    './images/chicken.jpg',
+    './images/cow.jpg',
+    './images/face.jpg',
+    './images/face2.jpg',
+    './images/fellowship.webp',
+    './images/food.jpg',
+    './images/handy.jpg',
+    './images/keys.png',
+    './images/lovepik.jpeg',
+    './images/mordor.webp',
+    './images/rocks.jpg',
+    './images/standing.jpeg',
+    './images/ties.jpg',
+    './images/tools.jpg',
+    './images/tools2.jpg',
+    './images/tow-mater.jpeg',
+    './images/trooper.jpg',
+    './images/witcher.webp'
 ]
 
-imageButton.addEventListener('click', function() {
+imageButton.addEventListener('click', function () {
     // for(var i=0;i<imgArray.length;i++) {
-        // var img = new Image();
-    if(currentIndex < imgArray.length) {
+    // var img = new Image();
+    if (currentIndex < imgArray.length) {
         currentIndex++;
-            imageEl = document.getElementById('background-image');
-            imageEl.src = imgArray[currentIndex]     
+        imageEl = document.getElementById('background-image');
+        imageEl.src = imgArray[currentIndex]
     } else {
         currentIndex = 0;
         var imageEl = document.getElementById('background-image');
-            imageEl.src = imgArray[currentIndex]
-    }       
+        imageEl.src = imgArray[currentIndex]
+    }
 })
 
-save.addEventListener('click', function() {
+save.addEventListener('click', function () {
     var imageToSave = imgArray[currentIndex];
     var JokeToSave = document.getElementById('dadJoke').textContent;
     fetch("/api/jokes", {
         method: "POST",
-        body: JSON.stringify({ 
+        body: JSON.stringify({
             image: imageToSave,
             dad_joke: JokeToSave
         })
     })
 })
 
-jokeBtn.addEventListener('click', function() {
+jokeBtn.addEventListener('click', function () {
     fetch("/generate/dadjoke")
-    .then(response => response.json())
-    .then(data => {
-        var dadJoke = document.getElementById("dadJoke");
-        dadJoke.textContent = data.joke;
-    })
+        .then(response => response.json())
+        .then(data => {
+            var dadJoke = document.getElementById("dadJoke");
+            dadJoke.textContent = data.joke;
+        })
 })
+
+
+
 
 
