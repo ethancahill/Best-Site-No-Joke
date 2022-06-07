780876840dd8a248ba85c9fb6a50a735110c22fe
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-07 15:43:58 -0400

    modified login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,21 @@
-const loginButton = document.getElementById('submit-login');
-const signUpButton = document.getElementById('submit-signup');
-
-
-const loginFormHandler = async event => {
-  event.preventDefault();
-  console.log(event);
-
-  const username = document.querySelector('#login-in').value.trim();
-  const password = document.querySelector('#login-pw').value.trim();
-
-  if (username && password) {
-    const response = await fetch('/api/users/', {
-      method: 'post',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/generate');
-    } else {
-      alert('Failed to log in.');
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#login-un').value.trim();
+    const password = document.querySelector('#login-pw').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/generate');
+      } else {
+        alert('Failed to log in.');
+      }
     }
   };
   
