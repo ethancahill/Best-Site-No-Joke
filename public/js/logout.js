9b41e7eb4a4c0fe1e5ecf20242ac3b34fedf347c
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-07 15:26:06 -0400

    got login and logout buttons working

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,11 +5,10 @@ const logout = async () => {
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace("/login");
   } else {
     alert("Failed to log out.");
   }
 };
 
 document.querySelector("#logout").addEventListener("click", logout);
-document.querySelector("#logout2").addEventListener("click", logout);
