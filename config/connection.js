bfe7ec66862981f1afbb3e344156682753efbaa2
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-08 13:57:29 -0400

    setup config for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    dialectOptions: "postgres",
   }
 );
 
