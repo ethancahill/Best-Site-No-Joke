d8e9e330a2a5030997e53274b7254fa3449f782e
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-06-09 15:57:08 -0400

    modified joke save feature to not allow empty dad joke to be saved

--- public/js/saveData.js
+++ public/js/saveData.js
@@ -1,29 +1,30 @@
-const saveButton = document.getElementById('save');
+const saveButton = document.getElementById("save");
 // const jokeBox = document.getElementById('joke-id');
 // const picture = document.getElementById('background-image-div');
 
-
 async function saveFormHandler(event) {
-    event.preventDefault();
-    console.log('clicked');
-    var image = imgArray[currentIndex].toString();
-    var dad_joke = document.getElementById('dadJoke').textContent;
+  event.preventDefault();
+  console.log("clicked");
+  var image = imgArray[currentIndex].toString();
+  var dad_joke = document.getElementById("dadJoke").textContent;
 
-
-    const response = await fetch('/api/jokes', {
-        method: 'post',
-        body: JSON.stringify({
-            dad_joke,
-            image,
-        }),
-        headers: { "Content-Type": "application/json" }
+  if (dad_joke === '') {
+    alert("Please select a dad joke");
+  } else {
+    const response = await fetch("/api/jokes", {
+      method: "post",
+      body: JSON.stringify({
+        dad_joke,
+        image,
+      }),
+      headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
-        alert('Joke/Image saved successfully')
+      alert("Joke/Image saved successfully");
     } else {
-        alert('Failed to save joke.');
+      alert("Failed to save joke.");
     }
-};
+  }
+}
 
-
-saveButton.addEventListener('click', saveFormHandler);
+saveButton.addEventListener("click", saveFormHandler);
